8826d1ac94bf94a0e8d8b35bea45bec769cb2229
prosincebirth <88553627+prosincebirth@users.noreply.github.com>
2021-10-03 19:14:45 +0800

    Create tab.php

---
+++ components/tab.php
@@ -0,0 +1,83 @@
+<section class="tab_section">
+    <div class="container">
+        <div class="tab_wrapper">
+            <ul class="tab_list__nav">
+                <li class="tab_nav_item active">
+                    <a href="#">Popular</a>
+                </li>
+                <li class="tab_nav_item">
+                    <a href="#">
+                        <i class="fa">&#xf11b;</i>
+                        <span clas="tab_title">DOTA 2</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="tab_contents">
+                <div class="" id="popular_tab">
+                    <div class="popular_items">
+                        <div class="popular_list__items">
+                            <div class="item_wrapper">
+                                <span class="item_tag is-purple">Mythical</span>
+                                <div class="img_wrapper">
+                                    <img src="http://via.placeholder.com/200x150" alt="">
+                                </div>
+                                <div class="item_info">
+                                    <h3 class="item_title">Staff sample</h3>
+                                    <span class="item_price">₱ 109.98</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="popular_list__items">
+                            <div class="item_wrapper">
+                                <span class="item_tag is-purple">Mythical</span>
+                                <div class="img_wrapper">
+                                    <img src="http://via.placeholder.com/200x150" alt="">
+                                </div>
+                                <div class="item_info">
+                                    <h3 class="item_title">Staff sample</h3>
+                                    <span class="item_price">₱ 109.98</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="popular_list__items">
+                            <div class="item_wrapper">
+                                <span class="item_tag is-purple">Mythical</span>
+                                <div class="img_wrapper">
+                                    <img src="http://via.placeholder.com/200x150" alt="">
+                                </div>
+                                <div class="item_info">
+                                    <h3 class="item_title">Staff sample</h3>
+                                    <span class="item_price">₱ 109.98</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="popular_list__items">
+                            <div class="item_wrapper">
+                                <span class="item_tag is-purple">Mythical</span>
+                                <div class="img_wrapper">
+                                    <img src="http://via.placeholder.com/200x150" alt="">
+                                </div>
+                                <div class="item_info">
+                                    <h3 class="item_title">Staff sample</h3>
+                                    <span class="item_price">₱ 109.98</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="popular_list__items">
+                            <div class="item_wrapper">
+                                <span class="item_tag is-purple">Mythical</span>
+                                <div class="img_wrapper">
+                                    <img src="http://via.placeholder.com/200x150" alt="">
+                                </div>
+                                <div class="item_info">
+                                    <h3 class="item_title">Staff sample</h3>
+                                    <span class="item_price">₱ 109.98</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
