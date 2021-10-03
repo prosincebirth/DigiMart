9b07a4643339746cb40596820d0e37552f9231b8
prosincebirth <88553627+prosincebirth@users.noreply.github.com>
2021-10-03 19:14:49 +0800

    Create head.php

---
+++ head.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DigiMart</title>
+    <link rel="preload stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" as="style" crossorigin="anonymous" />
+    <link rel="preload stylesheet" href="assets/css/reset.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/base.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/layout.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/button.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/form.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/header.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/mv.css" as="style" crossorigin>
+    <link rel="preload stylesheet" href="assets/css/tab.css" as="style" crossorigin>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+   <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+
+</head>
+<body>
