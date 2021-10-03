b5a4fe60af8a20e70e559138056c92337f9bc4e4
prosincebirth <88553627+prosincebirth@users.noreply.github.com>
2021-10-03 19:14:43 +0800

    Create mv.php

---
+++ components/mv.php
@@ -0,0 +1,20 @@
+<section class="mv_section">
+    <div class="mv_wrapper">
+        <div class="container">
+            <div class="main_slider">
+                <ul class="slider">
+                    <li class="slide_item">
+                        <h1>Hallowed Chest of Diretide</h1>
+                        <a href="">Trade it now</a>
+                    </li>
+                </ul>
+                <ul class="slide_nav">
+                    <li class="slide_nav__item"></li>
+                    <li class="slide_nav__item active"></li>
+                    <li class="slide_nav__item"></li>
+                    <li class="slide_nav__item"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
