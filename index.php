53879b24c3bd932df736995db0e1d0ce399ab7c3
prosincebirth <88553627+prosincebirth@users.noreply.github.com>
2021-10-03 19:14:53 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include_once('head.php'); ?>
+    <?php include_once('header.php'); ?>
+
+    <main>
+        <?php include 'components/mv.php'; ?>
+        <?php include 'components/tab.php'; ?>
+        <?php include_once('footer.php'); ?>
+
+    </main>
+
+</body>
+</html>
