c18d532be8b57b649b055eda5efb1689064caf31
prosincebirth <88553627+prosincebirth@users.noreply.github.com>
2021-10-08 22:06:19 +0800

    Update modal.php

--- components/modal.php
+++ components/modal.php
@@ -2,7 +2,7 @@
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h4 class="modal-title">Add New Game <span class="close">&times;</span></h4>
+				<h4 class="modal-title">Add Game <span class="close">&times;</span></h4>
 			</div>
 			<div class="modal-body">
             <form class="form-signin" method="post" id="add-game-form"><center>
@@ -83,18 +83,18 @@
 </div>
 
 
-<div id="add_game_modal">
+<div id="sale_item_modal">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h4 class="modal-title">Add Game Item<span class="close">&times;</span></h4>
+				<h4 class="modal-title">Post Item for Sale<span class="close">&times;</span></h4>
 			</div>
 			<div class="modal-body">
             <form class="form-signin" method="post" id="add-game-form"><center>
                 <div class="fld_input"><input type="text" name="game_name" placeholder="Game Name" id="game_name" class="form-control"></div>
 				<div class="fld_input"><input type="text" name="game_desc" placeholder="Game Description" id="game_desc" class="form-control"></div>
 				<div class="fld_input"><input type="text" name="game_region" placeholder="Game Region" id="game_region" class="form-control"></div>
-				<div class="fld_input"><input type="text" name="game_server" placeholder="Game Server" id="game_server" class="form-control"></div>
+				<div class="fld_input"><input type="file" name="game_server" placeholder="Game Server" id="game_server" class="form-control"></div>
 
             </div>
 			<div class="modal-footer">
@@ -115,7 +115,7 @@
 			<div class="modal-body">
             <form class="form-signin" method="post" id="add-game-form"><center>
                 <div class="fld_input"><input type="text" name="game_name" placeholder="Game Name" id="game_name" class="form-control"></div>
-				<div class="fld_input"><input type="text" name="game_desc" placeholder="Game Description" id="game_desc" class="form-control"></div>
+				<div class="fld_in 	put"><input type="text" name="game_desc" placeholder="Game Description" id="game_desc" class="form-control"></div>
 				<div class="fld_input"><input type="text" name="game_region" placeholder="Game Region" id="game_region" class="form-control"></div>
 				<div class="fld_input"><input type="text" name="game_server" placeholder="Game Server" id="game_server" class="form-control"></div>
 
