5537fdf0176d26e35822685ba4954f12b9a97e9d
Kasakuro Futako <78971598+Kasakuro@users.noreply.github.com>
2021-10-12 20:50:08 +0800

    global market

---
+++ global-market.php
@@ -0,0 +1,32 @@
+<?php include('head.php'); ?>
+<?php include('header.php'); ?>
+
+<main>
+    <section class="market_section">
+        <div class="container_full-width">
+            <div class="market_wrapper">
+                <div class="mainbar">
+                    <div class="market_item--wrapper">
+                        <div class="market_tabs">
+                            <ul class="market_tab--list">
+                                <li><span>Sale</span></li>
+                                <li><span>Purchase</span></li>
+                            </ul>
+                        </div>
+                        <div class="market_item--container market_popular--items">
+                            <div class="items_wrapper">
+                                <ul class='item_grid'>
+                                    <li class="item_grid--item">
+                                        <div class="item_card"></div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include('footer.php'); ?>
