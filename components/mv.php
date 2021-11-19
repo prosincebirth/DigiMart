f70da1e91f32fa586e5a772fc91d302870aace5d
Angelo Abella <angeloabella111@gmail.com>
2021-11-19 23:21:37 +0800

    slider sa homepage

--- components/mv.php
+++ components/mv.php
@@ -1,20 +1,50 @@
+<link rel="preload stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.5.4/dist/css/splide.min.css" as="style" crossorigin>
+<link rel="preload stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.5.4/dist/css/splide-core.min.css" as="style" crossorigin>
+
 <section class="mv_section">
     <div class="mv_wrapper">
-        <div class="container">
-            <div class="main_slider">
-                <ul class="slider">
-                    <li class="slide_item">
-                        <h1>Hallowed Chest of Diretide</h1>
-                        <a href="">Trade it now</a>
+        <div class="splide">
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <li class="splide__slide">
+                        <div class="splide_info_container">
+                            <div class="splide__img">
+                                <img src="https://via.placeholder.com/1280x720" alt="">
+                            </div>
+                            <div class="slide__info">
+                                <h1>Horn of the Ancient</h1>
+                                <a href="">Buy it now</a>
+                            </div>
+                        </div>
                     </li>
-                </ul>
-                <ul class="slide_nav">
-                    <li class="slide_nav__item"></li>
-                    <li class="slide_nav__item active"></li>
-                    <li class="slide_nav__item"></li>
-                    <li class="slide_nav__item"></li>
+                    <li class="splide__slide">
+                        <div class="splide_info_container">
+                            <div class="splide__img">
+                                <img src="https://via.placeholder.com/1280x720" alt="">
+                            </div>
+                            <div class="slide__info">
+                                <h1>Silver Crest of Radiant</h1>
+                                <a href="">Buy it now</a>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
     </div>
 </section>
+
+<script src="assets/js/splide.min.js" async></script>
+
+<script type="module">
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        perPage: 1,
+        autoplay: true,
+        arrows: false,
+        speed: 500,
+        width: '100%'
+    });
+
+    splide.mount();
+</script>
